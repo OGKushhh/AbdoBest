0192f802724f48702f82caee3f642b10c2143b7b
Abdelrahman <abdo_1111400@yahoo.com>
2026-04-30 06:47:42 +0300

    rework

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
       'module-resolver',
@@ -7,9 +7,17 @@ module.exports = {
         root: ['./src'],
         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
         alias: {
-          '@': './src'
-        }
-      }
-    ]
-  ]
+          '@': './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@navigation': './src/navigation',
+          '@services': './src/services',
+          '@storage': './src/storage',
+          '@hooks': './src/hooks',
+          '@assets': './assets',
+          '@constants': './src/constants',
+        },
+      },
+    ],
+  ],
 };
